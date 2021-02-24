158df23ef8f2e1a7a905595aad63ce49a63b0712
Anastasia <sonniy@list.ru>
2021-02-24 19:29:12 +0700

    Add files via upload

---
+++ turn/DZ/DZ/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(206, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Консультант 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 423);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(282, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 35);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Имя";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(22, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(447, 203);
+            this.label4.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(206, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(263, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Консультант 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(206, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(263, 35);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Консультант 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 423);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 81);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Запись в очередь";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 550);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ turn/DZ/DZ/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DZ
+{
+    public partial class Form1 : Form
+    {
+        Queue<Human> human=new Queue<Human>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateForm()
+        {
+            label4.Text = "";
+            int index = 1;
+            foreach (Human x in human)
+            {
+                label4.Text += " # " + index + " " + x.Name + "\n";
+                index++;
+            }
+        }
+
+        private void UpdateLabel( Label lab)
+        {
+
+           // lab.Text = "";
+            int step = 0;
+            if (human.Count > 0)
+            {
+                foreach (Human x in human)
+                {
+                    step++;
+                    if (step == 1)
+                    {
+                        lab.Text = " " + x.Name;
+                        break;
+                    }
+
+
+                }
+
+                human.Dequeue();
+            }
+
+            UpdateForm();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateLabel(label1);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UpdateLabel(label2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UpdateLabel(label3);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "")
+            {
+                human.Enqueue(new Human(textBox1.Text));
+                textBox1.Text = "";
+            }
+            UpdateForm();
+        }
+    }
+}
