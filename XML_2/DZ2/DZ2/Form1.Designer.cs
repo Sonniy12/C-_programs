5663edc138bd8fc03b2d0d65b812d568efb034f4
Anastasia <sonniy@list.ru>
2021-02-24 19:43:30 +0700

    Add files via upload

---
+++ XML_2/DZ2/DZ2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DZ2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(1019, 31);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(439, 924);
+            this.listBox1.TabIndex = 16;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(146, 291);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(318, 62);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Имя+Значение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(146, 203);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(318, 68);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Delete color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(497, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(470, 919);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(146, 115);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(318, 78);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(318, 74);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(146, 398);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(318, 62);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Очистить поля";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1485, 972);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ XML_2/DZ2/DZ2/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+/// <summary>
+/// Где искать ошибку с маркером недопустимым
+/// </summary>
+
+namespace DZ2
+{
+    public partial class Form1 : Form
+    {
+        XmlTextWriter shop = new XmlTextWriter("motorcycle_shop.xml", Encoding.Unicode);
+        public Form1()
+        {
+            InitializeComponent();
+            shop.Formatting = Formatting.Indented;
+            shop.WriteStartDocument();
+
+            shop.WriteStartElement("motorcycle_shop");
+            //1
+            shop.WriteStartElement("Новые_мотоциклы");
+            shop.WriteStartElement("Harley_Davidson");
+            shop.WriteAttributeString("Harley_Davidson_Image","https://hd-nsk.ru/collections/%D0%BD%D0%BE%D0%B2%D1%8B%D0%B5-%D0%BC%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB%D1%8B/products/cvo-limited-flhtkse-harley-davidson-2");
+
+            shop.WriteElementString("Изготовлено", "США");
+            shop.WriteElementString("Цвет", "Синий");
+            shop.WriteElementString("Год", "2020");
+            shop.WriteElementString("Model", "Ultra Limited FLHTKSE 117");
+            shop.WriteElementString("Цена", "1.800.000 ");
+            shop.WriteEndElement();
+
+            //2
+            shop.WriteStartElement("Мотоциклы_с_пробегом");
+            shop.WriteStartElement("Harley_Davidson");
+            shop.WriteAttributeString("Harley_Davidson_Image", "https://hd-nsk.ru/collections/%D0%BC%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB%D1%8B-%D1%81-%D0%BF%D1%80%D0%BE%D0%B1%D0%B5%D0%B3%D0%BE%D0%BC/products/v-rod-muscle-harley-davidson-2011");
+
+            shop.WriteElementString("Изготовлено", "США");
+            shop.WriteElementString("Цвет", "Белый");
+            shop.WriteElementString("Год", "2011");
+            shop.WriteElementString("Model", "V-Rod Muscle");
+            shop.WriteElementString("Цена", "1.000.000 ");
+            shop.WriteEndElement();
+            //3
+
+            shop.WriteStartElement("Мотоциклы_других_марок");
+            shop.WriteStartElement("Ducati_Diavel");
+            shop.WriteAttributeString("Image", "https://hd-nsk.ru/collections/%D0%BC%D0%BE%D1%82%D0%BE%D1%86%D0%B8%D0%BA%D0%BB%D1%8B-%D0%B4%D1%80%D1%83%D0%B3%D0%B8%D1%85-%D0%BC%D0%B0%D1%80%D0%BE%D0%BA/products/ducati-diavel-2012");
+
+            shop.WriteElementString("Изготовлено", "США");
+            shop.WriteElementString("Цвет", "Серый");
+            shop.WriteElementString("Год", "2012");
+            shop.WriteElementString("Model", "Ducati Diavel 2012");
+            shop.WriteElementString("Цена", "700.000");
+            shop.WriteEndElement();
+
+
+
+            shop.Close();
+
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load("motorcycle_shop.xml");
+
+                XmlNode node = doc.DocumentElement;
+
+                Show_Xml(node);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load("motorcycle_shop.xml");
+            XmlNode node = document.DocumentElement;
+            node.RemoveChild(node.FirstChild);
+
+            document.Save("motorcycle_shop2.xml");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load("motorcycle_shop2.xml");
+            XmlNode new_Node = document.DocumentElement;
+
+            DeleteColor(new_Node, "Цвет");
+            document.Save("Color_delet.xml");
+        }
+        private void DeleteColor(XmlNode obj, string color)
+        {
+            if (obj.HasChildNodes)
+            {
+                foreach (XmlNode item in obj.ChildNodes)
+                {
+                    DeleteColor(item, color);
+                    if (item.Name == color)
+                    {
+                        obj.RemoveChild(item);
+                    }
+                }
+            }
+
+
+
+        }
+
+       
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            XmlTextReader reader = new XmlTextReader("motorcycle_shop.xml");
+            reader.WhitespaceHandling = WhitespaceHandling.None;
+            while (reader.Read())
+            {
+                listBox1.Items.Add("+++++++++++++");
+                listBox1.Items.Add("Name =" + reader.Name);
+                listBox1.Items.Add("Value =" + reader.Value);
+                listBox1.Items.Add("Type =" + reader.NodeType);
+                listBox1.Items.Add("+++++++++++++");
+                if (reader.AttributeCount > 0)
+                {
+                    while (reader.MoveToNextAttribute())
+                    {
+                        listBox1.Items.Add("------------");
+                        listBox1.Items.Add("Name Atribut=" + reader.Name);
+                        listBox1.Items.Add("Value Atribut=" + reader.Value);
+                        listBox1.Items.Add("------------");
+                    }
+                }
+            }
+            reader.Close();
+
+        }
+
+      
+        private void Show_Xml(XmlNode node)
+        {
+            textBox1.Text += "Имя " + node.Name + "\n";
+            textBox1.Text += "  Value " + node.Value + "\n";
+            textBox1.Text += "  Type " + node.NodeType + "\n";
+            if (node.Attributes != null)
+            {
+                foreach (XmlAttribute item in node.Attributes)
+                {
+                    textBox1.Text += "    Атрибут value=" + item.Value + " ";
+                    textBox1.Text += "    Атрибут name=" + item.Name + "\n";
+
+                }
+            }
+            if (node.HasChildNodes)
+            {
+                foreach (XmlNode item in node.ChildNodes)
+                {
+                    Show_Xml(item);
+                }
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            textBox1.Text = "";
+        }
+    }
+}
