9b27ee2943d65254b72645e61deb437dc137a49d
Anastasia <sonniy@list.ru>
2021-02-24 19:20:40 +0700

    Add files via upload

---
+++ Names/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 400);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Очередь фамилии";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(385, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(300, 400);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Стэк Имена";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(37, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 170);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вывод";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Добавление";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(46, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Удаление";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(46, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 30);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Вывод очереди";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(38, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 170);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Вывод";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(56, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 30);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Добавить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(56, 113);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 30);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Удалить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(56, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(180, 30);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Вывод стэка";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Names/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Queue<Last_name> last_name = new Queue<Last_name>();
+        Stack<Name> name = new Stack<Name>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void UpdateForm()
+        {
+            label1.Text="";
+            int index = 1;
+            foreach (Last_name x in last_name)
+            {
+                label1.Text +=" # " + index + " " + x.L_Name+"\n";
+                index++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                last_name.Enqueue(new Last_name(textBox1.Text));
+                textBox1.Text = "";
+            }
+            UpdateForm();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (last_name.Count > 0)
+            {
+               last_name.Dequeue();
+              
+            }
+            UpdateForm();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            int index = 1;
+            foreach (Last_name x in last_name)
+            {
+                label1.Text += " # " + index + " " + x.L_Name;
+                index++;
+            }
+           
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                name.Push(new Name(textBox2.Text));
+                textBox2.Text = "";
+            }
+            UpdateForm_two();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (name.Count > 0)
+            {
+                name.Pop();
+
+            }
+            UpdateForm_two();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            int index = 1;
+            foreach (Name x in name)
+            {
+                label2.Text += " # " + index + " " + x.Names;
+                index++;
+            }
+
+        }
+        private void UpdateForm_two()
+        {
+            label2.Text = "";
+            int index = 1;
+            foreach (Name x in name)
+            {
+                label2.Text += " # " + index + " " + x.Names + "\n";
+                index++;
+            }
+        }
+    }
+}
